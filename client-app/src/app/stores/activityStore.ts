e8b4a3a59f288bd54408df0aae89186065084f1f
Fredrik Lam <fredrik.didi.lam@gmail.com>
2022-05-08 16:01:13 +0200

    Setting up Formik
    Validation with Formik
    Creating reusable:
    - Text input
    - Text area
    - Select input
    - Date input
    Date strategy
    Using Date-DNS

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
@@ -8,20 +9,20 @@ export default class ActivityStore {
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = true;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this)
     }
 
     get activitiesByDate() {
-        return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
+        return Array.from(this.activityRegistry.values()).sort((a, b) => a.date!.getTime() - b.date!.getTime())
     }
 
     get groupActivities() {
         return Object.entries(
             this.activitiesByDate.reduce((activities, activity) => {
-                const date = activity.date;
+                const date = format(activity.date!, 'dd MMM yyyy')
                 activities[date] = activities[date] ? [...activities[date], activity] : [activity];
                 return activities;
             }, {} as {[key: string]: Activity[]})
@@ -29,7 +30,7 @@ export default class ActivityStore {
     }
 
     private setActivity = (activity: Activity) => {
-        activity.date = activity.date.split('T')[0];  //"2022-07-29T14:29:11.8254242"
+        activity.date = new Date(activity.date!);
                 this.activityRegistry.set(activity.id, activity); // set(key, value) - set = add or edit
     }
 
