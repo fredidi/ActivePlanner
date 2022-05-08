dcb12230f19e613d18dbcc309a130ad37bb4e581
Fredrik Lam <fredrik.didi.lam@gmail.com>
2022-05-08 17:26:18 +0200

    Adding seed users

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using API;
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,8 +28,9 @@ namespace Reactivity
             try
             {
                 var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await Seed.SeedData(context, userManager);
             }
 
             catch (Exception ex) 
