c3ae20c796c2fd2717b9322f03d4e02cb39c67c9
Fredrik Lam <fredrik.didi.lam@gmail.com>
2022-05-09 19:57:13 +0200

    Finish client-app login and registration
    - Persisting the login (localStorage)
    - Add Modals (Login & Register)
    - Handling validation errors in Form

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -14,13 +14,14 @@ export default class UserStore {
     get isLoggedIn() {
         return !!this.user;
     }
-    
+
     login = async (creds: UserFormValues) => {
         try {
             const user = await agent.Account.login(creds);
             store.commonStore.setToken(user.token);
             runInAction(() => this.user = user);
             history.push('/activities');
+            store.modalStore.closeModal();
             console.log(user);
         }
         catch (error) {
@@ -33,5 +34,30 @@ export default class UserStore {
         window.localStorage.removeItem('jwt');
         this.user = null;
         history.push('/');
+    }
+
+    getUser = async () => {
+        try
+        {
+            const user = await agent.Account.current();
+            runInAction(() => this.user = user);
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    register = async (creds: UserFormValues) => {
+        try {
+            const user = await agent.Account.register(creds);
+            store.commonStore.setToken(user.token);
+            runInAction(() => this.user = user);
+            history.push('/activities');
+            store.modalStore.closeModal();
+            console.log(user);
+        }
+        catch (error) {
+            throw error;
+        }
     }
 }
