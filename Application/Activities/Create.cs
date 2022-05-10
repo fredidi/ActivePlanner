520db83fd55ba73f7e93d8ebf25ec6a7cec1d9be
Fredrik Lam <fredrik.didi.lam@gmail.com>
2022-05-10 15:33:19 +0200

    Continue Configuring new relationship
    - Added Infrastructure project
    - Updated create activity handler
    - Configuring AutoMapper
    - Added attendance handler
    - Added custom auth policy
    - Updated seed data with attendees (drop database and re-run)

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,9 +1,11 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -16,9 +18,9 @@ namespace Application.Activities
         }
 
         //MIDDLEWARE
-        public class CommanValidator : AbstractValidator<Command>
+        public class CommandValidator : AbstractValidator<Command>
         {
-            public CommanValidator()
+            public CommandValidator()
             {
                 RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
             }
@@ -27,13 +29,26 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
             }
 
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                var attendee = new ActivityAttendee
+                {
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true
+                };
+
+                request.Activity.Attendees.Add(attendee);
+
                 _context.Activities.Add(request.Activity);
 
                 var result = await _context.SaveChangesAsync() > 0;
